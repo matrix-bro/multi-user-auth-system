11fd64d161cd8b32d56557aa7e0a684be4f5dc1e
matrix-bro <razmhz11@gmail.com>
2024-04-08 22:07:38 +0545

    Done with User Registration

---
+++ app/forms.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'phone_no', 'password1', 'password2']
+    
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'John',
+        'class': INPUT_CLASSES
+    }))
+
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Doe',
+        'class': INPUT_CLASSES
+    }))
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'john',
+        'class': INPUT_CLASSES
+    }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'john@doe.com',
+        'class': INPUT_CLASSES
+    }))
+
+    phone_no = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '+977 987654321',
+        'class': INPUT_CLASSES
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class': INPUT_CLASSES
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm password',
+        'class': INPUT_CLASSES
+    }))
--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from app.forms import RegisterForm
+from django.shortcuts import redirect
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'app/register.html', {'form': form})
